be597e769bc5f718fa24e9bab9904f177183b1e9
nileravenell <43184722+nileravenell@users.noreply.github.com>
2019-07-23 16:35:31 -0700

    Add files via upload

--- BenefitInterfaceFileEditor-Alpha.js
+++ BenefitInterfaceFileEditor-Alpha.js
@@ -12,13 +12,14 @@ function handleFileSelect(event){
 
 function handleFileLoad(event){
   document.getElementById('fileContent').textContent = event.target.result;
+  let fileContent = String('fileContent')
 }
 
 //XMl Handles File Select
 function handleXMLFileSelect(event){
   const reader = new FileReader()
   reader.onload = handleXMLFileLoad;
-  reader.readAsText(event.target.files[0])   
+  reader.readAsText(event.target.files[0])
 }
 
 function handleXMLFileLoad(event){
@@ -27,15 +28,62 @@ function handleXMLFileLoad(event){
 
 //XML Parse
 function XMLParser(text){
+  document.getElementById("mount").innerHTML = null
   let parser = new DOMParser();
   let XMLDoc = parser.parseFromString(text,"text/xml");
   let stack = [XMLDoc];
+  let dateFormat;
+  let target;
+  let fieldIndex = 0;
   while (stack.length > 0) {
     let node = stack.pop();
     for (i = 0; node.children.length > i; i++){
-      stack.push(node.children[i]);
+      stack.push(node.children[node.children.length-i-1]);
     }
-    console.log(node.tagName)
+    switch(node.tagName){
+      case "interface":
+        headerNode = document.createElement("H6");
+        headerNode.innerHTML = node.getAttribute("name");
+        document.getElementById('mount').appendChild(headerNode);
+        break;
+      case "line":
+        lineNode = document.createElement("H1");
+        lineNode.innerHTML = node.getAttribute("type");
+        document.getElementById('mount').appendChild(lineNode);
+        break;
+      case "section":
+        sectionNode = document.createElement("H6");
+        sectionNode.innerHTML = node.getAttribute("name");
+        document.getElementById('mount').appendChild(sectionNode);
+        break;
+      case "dateFormat":
+        dateFormat = node.innerHTML;
+        console.log(dateFormat);
+        break;
+      case "fields":
+        /*fieldsNode = document.createElement("ul");
+        fieldsNode.id = "fields" + String(fieldIndex);
+        fieldIndex++;
+        document.getElementById('mount').appendChild(fieldsNode);
+        target = fieldsNode;*/
+        break;
+      case "field":
+        divNode = document.createElement("div");
+        divNode.class = "form-group";
+        labelNode = document.createElement("label");
+        inputNode = document.createElement("input");
+        inputNode.class = "form-control";
+        divNode.appendChild(labelNode);
+        divNode.appendChild(inputNode);
+        document.getElementById('mount').appendChild(divNode);
+        labelNode.innerHTML = node.getAttribute("name");
+        inputNode.id = "fields" + String(fieldIndex);
+        labelNode.for = inputNode.id
+        inputNode.maxLength = parseInt(node.getAttribute("length"), 10);
+        /*fieldNode = document.createElement("li");
+        fieldNode.innerHTML = node.getAttribute("name");
+        document.getElementById(target.id).appendChild(fieldNode);*/
+    }
   }
 }
 
@@ -45,3 +93,5 @@ function XMLContent(){
   var attr = elmnt.getAttributeNode('class').value;
   document.getElementById("XMLInput").innerHTML = attr;
 }
+
+//XML Display
